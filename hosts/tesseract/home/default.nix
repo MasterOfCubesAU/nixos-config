caff5c0a3b0284d5b8a6dbc0672dbd7f6fa82e28
MasterOfCubesAU <masterofcubesau@gmail.com>
2025-06-04 10:30:31 +1000

    fix: remove steam from tessereact home config

--- hosts/tesseract/home/default.nix
+++ hosts/tesseract/home/default.nix
@@ -6,14 +6,5 @@
   home = {
     username = "brandon";
     homeDirectory = "/home/brandon";
-
-  };
-
-  # Steam
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
   };
 }
