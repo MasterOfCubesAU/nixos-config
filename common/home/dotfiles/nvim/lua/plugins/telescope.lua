903d40d086c3638e70dd935158015981647799ea
MasterOfCubesAU <masterofcubesau@gmail.com>
2025-06-20 21:06:32 +1000

    fix: fixed grep search and changed telescope bindings

--- common/home/dotfiles/nvim/lua/plugins/telescope.lua
+++ common/home/dotfiles/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,11 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' },
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
+  }
+}
